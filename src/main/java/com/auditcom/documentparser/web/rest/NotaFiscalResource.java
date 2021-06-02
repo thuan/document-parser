1b921d4cd44c0b357abb157754afea05fe0a724c
Thuan Saraiva <thuan.saraiva@pathfind.com.br>
2021-06-02 17:13:28 -0300

    final version

--- src/main/java/com/auditcom/documentparser/web/rest/NotaFiscalResource.java
+++ src/main/java/com/auditcom/documentparser/web/rest/NotaFiscalResource.java
@@ -13,27 +13,24 @@ import io.minio.errors.MinioException;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import tech.jhipster.web.util.HeaderUtil;
 
 /**
  * NotaFiscalResource controller
@@ -44,11 +41,6 @@ public class NotaFiscalResource {
 
     private final Logger log = LoggerFactory.getLogger(NotaFiscalResource.class);
 
-    private static final String ENTITY_NAME = "notaFiscalResource";
-
-    @Value("${jhipster.clientApp.name}")
-    private String applicationName;
-
     @Autowired
     private NotaFiscalEntradaService notaFiscalEntradaService;
 
@@ -56,11 +48,9 @@ public class NotaFiscalResource {
     private ProdutoEntradaService produtoEntradaService;
 
     @PostMapping(value = "/parsear", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<NotaFiscalEntradaDTO> parsear(@RequestParam(value = "files") MultipartFile[] files)
-        throws URISyntaxException, InvalidKeyException, NoSuchAlgorithmException, IOException {
-        NotaFiscalEntradaDTO entradaDTO = new NotaFiscalEntradaDTO();
-        ProdutoEntradaDTO produtoEntradaDTO = new ProdutoEntradaDTO();
-        log.debug("REST request to send XML Parser: {}", entradaDTO, produtoEntradaDTO);
+    public List<NotaFiscalEntradaDTO> parsear(@RequestParam(value = "files") MultipartFile[] files)
+        throws InvalidKeyException, NoSuchAlgorithmException, IOException {
+        List<NotaFiscalEntradaDTO> lstNotas = new ArrayList<>();
         try {
             String accessKey = "minioadmin";
             String secretKey = "minioadmin";
@@ -68,6 +58,9 @@ public class NotaFiscalResource {
             MinioClient minioClient = MinioClient.builder().endpoint("http://127.0.0.1:9000").credentials(accessKey, secretKey).build();
 
             for (MultipartFile file : files) {
+                NotaFiscalEntradaDTO entradaDTO = new NotaFiscalEntradaDTO();
+                ProdutoEntradaDTO produtoEntradaDTO = new ProdutoEntradaDTO();
+
                 Path tempFile = Files.createTempFile("teste", ".xml");
                 try (InputStream in = file.getInputStream()) {
                     Files.copy(in, tempFile, StandardCopyOption.REPLACE_EXISTING);
@@ -112,36 +105,37 @@ public class NotaFiscalResource {
 
                 entradaDTO = saveDto;
 
-                UploadObjectArgs.Builder builder = UploadObjectArgs
+                UploadObjectArgs.Builder builderXml = UploadObjectArgs
                     .builder()
                     .bucket("notas-xml")
                     .object(entradaDTO.getId() + ".xml")
                     .filename(tempFile.toString());
 
-                minioClient.uploadObject(builder.build());
+                minioClient.uploadObject(builderXml.build());
 
                 ObjectMapper mapper = new ObjectMapper();
                 File fileJson = new File("entrada.json");
                 mapper.writeValue(fileJson, entradaDTO);
 
-                UploadObjectArgs.Builder builder2 = UploadObjectArgs
+                UploadObjectArgs.Builder builderJson = UploadObjectArgs
                     .builder()
                     .bucket("notas-json")
                     .object(entradaDTO.getId() + ".json")
                     .filename(fileJson.toString());
 
-                minioClient.uploadObject(builder2.build());
+                minioClient.uploadObject(builderJson.build());
 
                 Files.delete(tempFile);
                 Files.delete(fileJson.toPath());
+
+                log.debug("REST request to send XML Parser: {}", entradaDTO, produtoEntradaDTO);
+
+                lstNotas.add(entradaDTO);
             }
         } catch (JAXBException | MinioException e) {
             e.printStackTrace();
         }
 
-        return ResponseEntity
-            .created(new URI("/api/nota-fiscal/enviar/" + entradaDTO.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert(applicationName, false, ENTITY_NAME, entradaDTO.getId().toString()))
-            .body(entradaDTO);
+        return lstNotas;
     }
 }
